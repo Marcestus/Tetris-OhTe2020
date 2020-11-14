9d0ac9e3fa3f94a261a5aa621269811e06f6a3b2
Marcestus <55045740+Marcestus@users.noreply.github.com>
2020-11-14 23:50:55 +0200

    projektin alustusta ja toimivuuden testausta

---
+++ Tetris/src/main/java/tetris/ui/TetrisUi.java
@@ -0,0 +1,41 @@
+package tetris.ui;
+
+
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Properties;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class TetrisUi extends Application{
+    
+    @Override
+    public void init() throws Exception {
+        //
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    
+}
