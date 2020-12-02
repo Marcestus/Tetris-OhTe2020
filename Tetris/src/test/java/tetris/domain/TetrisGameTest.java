6e1d97282e250d30136ccf442108b0b66644bbeb
Marcestus <55045740+Marcestus@users.noreply.github.com>
2020-12-02 16:03:54 +0200

    testejä lisätty

--- Tetris/src/test/java/tetris/domain/TetrisGameTest.java
+++ Tetris/src/test/java/tetris/domain/TetrisGameTest.java
@@ -19,9 +19,7 @@ public class TetrisGameTest {
     @Test
     public void ActiveShapeMovesCorrectlyToLeft() {
         TetrisGame game = new TetrisGame(0);
-        game.getActiveShape();
         Tile[] tiles = game.getActiveShapeTiles();
-        game.getPassiveShapes();
         game.moveLeft();
         assertEquals(4, tiles[0].getX());
         assertEquals(5, tiles[1].getX());
@@ -32,9 +30,7 @@ public class TetrisGameTest {
     @Test
     public void ActiveShapeMovesCorrectlyToRight() {
         TetrisGame game = new TetrisGame(0);
-        game.getActiveShape();
         Tile[] tiles = game.getActiveShapeTiles();
-        game.getPassiveShapes();
         game.moveRight();
         assertEquals(6, tiles[0].getX());
         assertEquals(7, tiles[1].getX());
@@ -45,13 +41,44 @@ public class TetrisGameTest {
     @Test
     public void ActiveShapeMovesCorrectlyToDown() {
         TetrisGame game = new TetrisGame(0);
-        game.getActiveShape();
         Tile[] tiles = game.getActiveShapeTiles();
-        game.getPassiveShapes();
         game.moveDown();
         assertEquals(2, tiles[0].getY());
         assertEquals(1, tiles[1].getY());
         assertEquals(2, tiles[2].getY());
         assertEquals(2, tiles[3].getY());
+    }
+    
+    @Test
+    public void LShapeRotatesCorrectly() {
+        TetrisGame game = new TetrisGame(0);
+        Tile[] tiles = game.getActiveShapeTiles();
+        game.rotate();
+        assertEquals(5, tiles[0].getX());
+        assertEquals(6, tiles[1].getX());
+        assertEquals(5, tiles[2].getX());
+        assertEquals(5, tiles[3].getX());
+        
+        assertEquals(1, tiles[0].getY());
+        assertEquals(2, tiles[1].getY());
+        assertEquals(0, tiles[2].getY());
+        assertEquals(2, tiles[3].getY());
+    }
+    
+    @Test
+    public void IShapeRotatesCorrectly() {
+        TetrisGame game = new TetrisGame(5);
+        Tile[] tiles = game.getActiveShapeTiles();
+        game.moveDown();
+        game.rotate();
+        assertEquals(5, tiles[0].getX());
+        assertEquals(5, tiles[1].getX());
+        assertEquals(5, tiles[2].getX());
+        assertEquals(5, tiles[3].getX());
+        
+        assertEquals(2, tiles[0].getY());
+        assertEquals(0, tiles[1].getY());
+        assertEquals(1, tiles[2].getY());
+        assertEquals(3, tiles[3].getY());
     }
 }
